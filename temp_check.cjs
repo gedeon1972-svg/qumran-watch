06b049361b91934368aa38ab71745dd340c34587
Mash Jayt <gedeon1972@gmail.com>
2026-06-29 10:17:44 -0300

    chore: remove temp file temp_check.cjs

--- temp_check.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/js/ui/mishmar-dashboard.js', 'utf8');
-console.log(content.length);
-console.log(content.includes('Qumr'));
-console.log(content.indexOf('Qumr'));
