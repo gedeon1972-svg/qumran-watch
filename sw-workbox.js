a30b5bff5a2f99ee1b6de4d64bf82ff9e9dbf7bc
gedeon1972-svg <gedeon1972-svg@users.noreply.github.com>
2026-08-10 18:57:31 +0000

    deploy: 12998b333cbe096ce668dd34c348911f512aed73

--- sw-workbox.js
+++ sw-workbox.js
@@ -1,4 +1,4 @@
-﻿// sw-workbox.js - Workbox Service Worker para Qumran Watch v13.1.61
+// sw-workbox.js - Workbox Service Worker para Qumran Watch v13.1.63
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.1.0/workbox-sw.js');
 
 const { precacheAndRoute, cleanupOutdatedCaches } = workbox.precaching;
@@ -7,7 +7,7 @@ const { NetworkFirst, CacheFirst, StaleWhileRevalidate } = workbox.strategies;
 const { ExpirationPlugin } = workbox.expiration;
 const { CacheableResponsePlugin } = workbox.cacheableResponse;
 
-console.log('[SW] Workbox SW v13.1.61 iniciando...');
+console.log('[SW] Workbox SW v13.1.63 iniciando...');
 
 precacheAndRoute(self.__WB_MANIFEST || []);
 cleanupOutdatedCaches();
@@ -176,4 +176,4 @@ async function showDueNotifications() {
     }
 }
 
-console.log('[SW] Workbox SW v13.1.61 listo');
+console.log('[SW] Workbox SW v13.1.63 listo');
