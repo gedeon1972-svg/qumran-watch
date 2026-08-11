d552e863ecef2f8562ab574c0dfa26875ed4edbd
gedeon1972-svg <gedeon1972-svg@users.noreply.github.com>
2026-08-11 13:36:40 +0000

    deploy: 75853be97bb8612710a3e7a271bdfd989dbeebac

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// sw-workbox.js - Workbox Service Worker para Qumran Watch v13.1.73
+// sw-workbox.js - Workbox Service Worker para Qumran Watch v13.1.74
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.1.0/workbox-sw.js');
 
 const { precacheAndRoute, cleanupOutdatedCaches } = workbox.precaching;
@@ -7,16 +7,16 @@ const { NetworkFirst, CacheFirst, StaleWhileRevalidate } = workbox.strategies;
 const { ExpirationPlugin } = workbox.expiration;
 const { CacheableResponsePlugin } = workbox.cacheableResponse;
 
-console.log('[SW] Workbox SW v13.1.73 iniciando...');
+console.log('[SW] Workbox SW v13.1.74 iniciando...');
 
 precacheAndRoute(
     [
         { revision: '830bb116a513550c5858d60ded660753', url: 'privacy.html' },
-        { revision: 'e6a3b2b612cd301941582fa8c9c46584', url: 'manifest.json' },
+        { revision: '46353993195feeb7aea4bcb5f6792c55', url: 'manifest.json' },
         { revision: 'c9572888756e5c887d1b56b6dff80e51', url: 'license.html' },
         { revision: 'b1c08f6dc2ed774946e83827340ab29b', url: 'index.html' },
         { revision: 'f214d4ac2c7f2e2c94e366ca34c5c92e', url: 'icon.png' },
-        { revision: '7aff424cc4d2c3063828f8da77074a37', url: 'src/js/index.js' },
+        { revision: 'd020b9576ca3cd7d0cbe2bc334d46c13', url: 'src/js/index.js' },
         { revision: 'ca6822b1a76e1cf6e01c00acaa3a61eb', url: 'src/js/core.js' },
         { revision: '961b051e8832d3d48163b7649f872924', url: 'src/css/index.css' },
         { revision: '2d8904c9b0cd7cb2929d0bb613047f58', url: 'src/css/fonts/david-libre-v17-latin-regular.woff2' },
@@ -200,4 +200,4 @@ async function showDueNotifications() {
     }
 }
 
-console.log('[SW] Workbox SW v13.1.73 listo');
+console.log('[SW] Workbox SW v13.1.74 listo');
